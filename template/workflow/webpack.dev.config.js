375951b7e704b68b927d560784fb79dd61965ee3
fishfan <fishf3@163.com>
2021-10-28 00:26:28 +0800

    增加HMR

--- template/workflow/webpack.dev.config.js
+++ template/workflow/webpack.dev.config.js
@@ -1,8 +1,16 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.base.config');
-
+const path = require('path');
 const devConfig = {
   mode: 'development',
   devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, './dist'),
+    open: false,
+    hot: true,
+    quiet: true,
+    port: 8082,
+  },
 };
 module.exports = merge(commonConfig, devConfig);
