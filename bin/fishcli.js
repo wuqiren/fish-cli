fbd69c5f9521e099312a1b181fb86c4ec86b0318
fishfan <fishfan@tencent.com>
2021-12-10 11:50:16 +0800

    增加vite模板

--- bin/fishcli.js
+++ bin/fishcli.js
@@ -5,25 +5,36 @@ const create = require('../src/create');
 const consoleColors = require('../utils');
 const question = [
   {
-    name: 'conf' /* key */,
-    type: 'confirm' /* 确认 */,
-    message: '是否创建新的项目？' /* 提示 */,
+    name: "conf" /* key */,
+    type: "confirm" /* 确认 */,
+    message: "是否创建新的项目？" /* 提示 */,
   },
   {
-    name: 'name',
-    message: '请输入项目名称？',
+    name: "name",
+    message: "请输入项目名称？",
     when: (res) => Boolean(res.conf) /* 是否进行 */,
   },
   {
-    name: 'author',
-    message: '请输入作者？',
+    name: "author",
+    message: "请输入作者？",
     when: (res) => Boolean(res.conf),
   },
   {
-    type: 'list' /* 选择框 */,
-    message: '请选择公共管理状态,抱歉没得选，只有zustand一个^_^',
-    name: 'state',
-    choices: ['zustand'] /* 选项*/,
+    type: "list" /* 选择框 */,
+    message: "请选择你的构建工具",
+    name: "type",
+    choices: ["webpack", "vite"] /* 选项*/,
+    filter: function (val) {
+      /* 过滤 */
+      return val.toLowerCase();
+    },
+    when: (res) => Boolean(res.conf),
+  },
+  {
+    type: "list" /* 选择框 */,
+    message: "请选择公共管理状态,抱歉没得选，只有zustand一个^_^",
+    name: "state",
+    choices: ["zustand"] /* 选项*/,
     filter: function (val) {
       /* 过滤 */
       return val.toLowerCase();
