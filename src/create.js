872e3534ac6bd2f9310c84972af0bd792f26477a
fishfan <fishfan@tencent.com>
2022-01-10 17:27:16 +0800

    把fs.exists方法切换为fs.access,同时创建一个总的项目文件夹

--- src/create.js
+++ src/create.js
@@ -2,31 +2,28 @@ const fs = require('fs');
 const { Buffer } = require('buffer');
 const utils = require('../utils');
 
-/* 三变量判断异步操作 */
-let fileCount = 0; /* 文件数量 */
-let dirCount = 0; /* 文件夹数量 */
-let flat = 0; /* readir数量 */
-
 module.exports = function (res) {
   /* 创建文件 */
   utils.green('------开始构建-------');
   const sourcePath = __dirname.slice(0, -3) + `${res.type}-template`;
   utils.blue('当前路径:' + process.cwd());
   /* 修改package.json*/
-  revisePackageJson(res, sourcePath).then(() => {
-    copy(sourcePath, process.cwd());
+  fs.mkdir(res.name, () => {
+    const path = process.cwd()+`/${res.name}`
+    revisePackageJson(res, sourcePath,path+'/package.json').then(() => {
+      copy(sourcePath, path);
+      utils.yellow('创建文件夹：' + res.name);
+    });
   });
 };
 
 const copy = (sourcePath, currentPath)=> {
-  flat++;
   fs.readdir(sourcePath, (err, paths) => {
-    flat--;
     if (err) {
       throw err;
     }
     paths.forEach((path) => {
-      if (path !== '.git' && path !== 'package.json') fileCount++;
+      if (path !== '.git' && path !== 'package.json');
       const newSoucePath = sourcePath + '/' + path;
       const newCurrentPath = currentPath + '/' + path;
       fs.stat(newSoucePath, (err, stat) => {
@@ -38,10 +35,8 @@ const copy = (sourcePath, currentPath)=> {
           const writeSteam = fs.createWriteStream(newCurrentPath);
           readSteam.pipe(writeSteam);
           utils.green('创建文件：' + newCurrentPath);
-          fileCount--;
         } else if (stat.isDirectory()) {
           if (path !== '.git' && path !== 'package.json') {
-            dirCount++;
             dirExist(newSoucePath, newCurrentPath, copy);
           }
         }
@@ -51,13 +46,11 @@ const copy = (sourcePath, currentPath)=> {
 }
 
 const dirExist =(sourcePath, currentPath, copyCallback)=> {
-  fs.exists(currentPath,(exist) => {
-    if (exist) {
+  fs.access(currentPath,fs.constants.F_OK,(error) => {
+    if (!error) {
       copyCallback(sourcePath, currentPath);
     } else {
       fs.mkdir(currentPath, () => {
-        fileCount--;
-        dirCount--;
         copyCallback(sourcePath, currentPath);
         utils.yellow('创建文件夹：' + currentPath);
       });
@@ -65,14 +58,13 @@ const dirExist =(sourcePath, currentPath, copyCallback)=> {
   });
 }
 
-const revisePackageJson = (res, sourcePath) => new Promise((resolve) => {
+const revisePackageJson = (res, sourcePath, path) => new Promise((resolve) => {
   fs.readFile(sourcePath + '/package.json', (err, data) => {
     if (err) throw err;
     const { author, name } = res;
     let json = data.toString();
     json = json.replace(/demoname/g, name.trim());
     json = json.replace(/demoAuthor/g, author.trim());
-    const path = process.cwd() + '/package.json';
     const data1 = new Uint8Array(Buffer.from(json));
     fs.writeFile(path, data1, () => {
       utils.green('创建文件：' + path);
